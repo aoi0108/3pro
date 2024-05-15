cc16e35c9c1edc6fb7101da8474d215c98f241c7
Aoi <109831244+aoi0108@users.noreply.github.com>
2024-05-15 13:14:27 +0900

    Add files via upload

---
+++ ShakeUp-master/ShakeUp/SwiftUI/ShakeTimesListView.swift
@@ -0,0 +1,31 @@
+//
+//  ShakeTimesListView.swift
+//  ShakeUp
+//
+//  Created by Hal Stroemeria on 2019/12/05.
+//  Copyright © 2019 hal-cha-n. All rights reserved.
+//
+
+import SwiftUI
+
+struct ShakeTimesListView: View {
+    @EnvironmentObject var shakeTimes: ShakeTimesViewModel
+    
+    var body: some View {
+        List {
+            ForEach(shakeTimes.shakeTimes) { shakeTime in
+                ShakeTimeView(shakeTime: shakeTime)
+            }
+        }
+        .sheet(isPresented: $shakeTimes.isPresentingShakeUpView) {
+            ShakeUpView()
+        }
+    }
+}
+
+struct ShakeTimesListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShakeTimesListView()
+            .environmentObject(ShakeTimesViewModel())
+    }
+}
