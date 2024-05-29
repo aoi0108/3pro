2a264acf2b083fe6c1f5c9d404cfeb179c87bfd1
mimi-program <keicat1234@gmail.com@gmail.com>
2024-05-29 13:21:25 +0900

    stopAlarmView作ったけど失敗

---
+++ StepsAlarmClock-main/AlarmClock/StopAlarmView.swift
@@ -0,0 +1,18 @@
+//
+//  StopAlarmView.swift
+//  AlarmClock
+//
+//  Created by 羽田野真央 on 2024/05/28.
+//
+
+import SwiftUI
+
+struct StopAlarmView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StopAlarmView()
+}
