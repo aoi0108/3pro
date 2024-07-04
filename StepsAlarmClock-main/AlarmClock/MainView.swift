8fb5d2d5a5af9a7e7edbe23c83db16c6cc824881
aoi0108 <hira.aoi.18@gmail.com>
2024-07-05 07:06:02 +0900

    add image

--- StepsAlarmClock-main/AlarmClock/MainView.swift
+++ StepsAlarmClock-main/AlarmClock/MainView.swift
@@ -17,7 +17,10 @@ struct MainView: View {
                 Text("レベル： " + String(correctCount) )
                     .font(.title)
                     .padding()
-                Image("neko")
+                Image(selectImageName(for: correctCount))
+                    .resizable() // 画像のリサイズを可能にする
+                    .aspectRatio(contentMode: .fit) // アスペクト比を維持
+                    .frame(width: 200, height: 200) // フレームサイズを設定
                     .padding()
                 HStack {
                     NavigationLink(destination: ContentView(correct: $correct)) {
@@ -38,6 +41,50 @@ struct MainView: View {
         }
         
     }
+    
+    func selectImageName(for count: Int) -> String {
+         switch count {
+         case 0:
+             return "image/coffeecup"
+         case 1:
+             return "image/coffee-jelly"
+         case 2:
+             return "image/end-jelly"
+         case 3:
+             return "image/coffeecup"
+         case 4:
+             return "image/pudding"
+         case 5:
+             return "image/pudding-cream"
+         case 6:
+             return "image/pudding-end"
+         case 7:
+             return "image/sodacup"
+         case 8:
+             return "image/soda-ice"
+         case 9:
+             return "image/soda-soda"
+         case 10:
+             return "image/soda-float"
+         case 11:
+             return "image/soda-fin"
+         case 12:
+             return "image/sodacup"
+         case 13:
+             return "image/parfeit-cereal"
+         case 14:
+             return "image/parfeit-ice"
+         case 15:
+             return "image/parfeit-cream"
+         case 16:
+             return "image/parfeit-berry"
+         case 17:
+             return "image/parfeit-fin"
+             
+         default:
+             return "neko"
+         }
+     }
 }
 
 #Preview {
